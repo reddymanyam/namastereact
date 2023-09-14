49c5188a9c5115c479d41c0069a39e7d558cfc1d
reddymanyam <142713295+reddymanyam@users.noreply.github.com>
2023-09-14 14:29:30 +0530

    Update inception.js

---
+++ inception.js
@@ -0,0 +1,5 @@
+const heading1 = React.createElement("h1",{id:"title"},"heading1");
+const heading2 = React.createElement("h2",{id:"title"},"heading2");
+const container = React.createElement("div",{id:"container"},[heading1,heading2]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
